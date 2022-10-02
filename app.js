746d58e4e678bc17da2682535f968bd5932bea42
Rishabh Chopade <ugamicstudios@gmail.com>
2022-10-02 13:19:17 +0530

    modification 4

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const route1 = require("./Routes/route1");
 
 const app = express();
+app.use(bodyParser.json());
 
-// this command will start the server.
-app.listen(5000);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
+app.use("/", route1);
+
+mongoose
+  .connect(
+    "mongodb+srv://teamproject1516:e9TOIJH7KsGzzkOQ@cluster0.nqkwh.mongodb.net/BikeRental"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
