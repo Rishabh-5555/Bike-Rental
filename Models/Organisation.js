4f729ee1b745f58c870b86b1809eb554ad9f5e3f
Rishabh Chopade <39895578+Rishabh-5555@users.noreply.github.com>
2022-10-05 19:22:09 +0530

    Update Organisation.js

--- Models/Organisation.js
+++ Models/Organisation.js
@@ -1,1 +1,9 @@
 const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const organisationSchema = new Schema({
+  name: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Organisation", organisationSchema);
