d1849eb27ebc5d73741226d559575a0787bf07ab
Rishabh Chopade <39895578+Rishabh-5555@users.noreply.github.com>
2022-10-31 10:43:26 +0530

    Update route1.js

--- Routes/route1.js
+++ Routes/route1.js
@@ -6,4 +6,8 @@ router.get("/new", (req, res, next) => {
   res.json("This is basic request");
 });
 
+router.post("/new", (req, res, next) => {
+ console.log(req.body);
+});
+
 module.exports = router;
