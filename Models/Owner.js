18af591098bff7c2388ddd6aef3129ee42ca0feb
Rishabh Chopade <39895578+Rishabh-5555@users.noreply.github.com>
2023-02-05 20:01:56 +0530

    Update Owner.js

--- Models/Owner.js
+++ Models/Owner.js
@@ -1,4 +1,30 @@
 const mongoose = require("mongoose");
 
 const workerSchema = new mongoose.Schema({
+  ownerName: {
+    type: String,
+    required: true,
+    min: 3,
+    max: 20,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 50,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+  },
+  isAvatarImageSet: {
+    type: Boolean,
+    default: false,
+  },
+  avatarImage: {
+    type: String,
+    default: "",
+  },
 })
